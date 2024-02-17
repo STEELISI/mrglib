f8402d2a794f4429476f4dfb7c3177965cecfa08
Jelena Mirkovic <mirkovic@isi.edu>
2024-02-17 21:10:54 +0000

    removed some files and fixed SSH

--- src/mrglib/Experiment.py
+++ src/mrglib/Experiment.py
@@ -155,7 +155,7 @@ class Experiment:
     # Execute command on node using SSH
     def exec_on_node(self, username, node, cmd):
         ssh_client = paramiko.SSHClient()
-        private_key = paramiko.RSAKey.from_private_key_file("/home/shareaa/.ssh/merge_key") #("/home/"+username+"/.ssh/merge_key")
+        private_key = paramiko.RSAKey.from_private_key_file("/home/"+username+"/.ssh/merge_key")
         ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         ssh_client.connect(node, username=username, pkey=private_key)
         (stdin, stdout, stderr) = ssh_client.exec_command(cmd)
